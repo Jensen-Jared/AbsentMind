65690abb404718aac9aad7d7412fa8ddfefbbaab
Dave Irwin <david@redirwin.com>
2017-01-25 11:54:50 -0700

    1/25/17 Added Game class

---
+++ AbsentMind/src/absentmind/AbsentMind.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package absentmind;
+
+/**
+ *
+ * @author Josh
+ */
+public class AbsentMind {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Player playerOne = new Player();
+        
+        playerOne.setName("John Doe");
+        playerOne.setBestTime(7.00);
+        playerOne.setEndingType("You died.");
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+        
+        Game game = new Game();
+        
+        game.setCurrentRow(2);
+        game.setCurrentColumn(5);
+        game.setTimeExpired(12);
+        game.setTimeRemaining(57);
+        
+        String gameInfo = game.toString();
+        System.out.println(gameInfo);
+        
+        
+    }
+    
+}
