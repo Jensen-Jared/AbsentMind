05000f7b754383a214a16a4f3aaf0272d48c6f16
Kailey <dil11004@byui.edu>
2017-01-25 14:28:55 -0700

    1/25/17 - Cleaned up Player Class, created Scene and Clue

---
+++ AbsentMind/src/absentmind/Clue.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package absentmind;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Josh
+ */
+public class Clue implements Serializable{
+    
+    private String clueName;
+    private String clueType;
+    private String description;
+
+    public Clue() {
+    }
+    
+    
+
+    public String getClueName() {
+        return clueName;
+    }
+
+    public void setClueName(String clueName) {
+        this.clueName = clueName;
+    }
+
+    public String getClueType() {
+        return clueType;
+    }
+
+    public void setClueType(String clueType) {
+        this.clueType = clueType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.clueName);
+        hash = 97 * hash + Objects.hashCode(this.clueType);
+        hash = 97 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Clue{" + "clueName=" + clueName + ", clueType=" + clueType + ", description=" + description + '}';
+    }
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Clue other = (Clue) obj;
+        if (!Objects.equals(this.clueName, other.clueName)) {
+            return false;
+        }
+        if (!Objects.equals(this.clueType, other.clueType)) {
+            return false;
+        }
+        return Objects.equals(this.description, other.description);
+    }
+    
+    
+    
+    
+    }
+  
